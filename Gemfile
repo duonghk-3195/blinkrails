b91c95c5b8e1f621fc316e144a919e62d3c55c05
duongplayer99 <ho.khanh.duong99@gmail.com>
2024-01-23 17:47:46 +0900

    chuong 6

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'bootstrap-sass', '3.4.1'
+
+gem 'bcrypt', '3.1.13'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
     has_many :post
+
+    before_save :downcase_email
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :name, presence: true, length: { minium: 5, maximum: 50}
+    validates :password, presence: true, length: { minimum: 6 }, 
+        if: :password # chi thay doi password khi co su thay doi
+    has_secure_password # ho tro viec xu ly mat khau
+    validates :email, presence: true, length: { minium: 20, maximum: 255, }, 
+        format: {with: VALID_EMAIL_REGEX}, # check dinh dang email
+        uniqueness: {case_sensitive: false} # thuoc tinh email la duy nhat, khi them opstion scope: :group_id thi co the check unique theo từng group
+
+    private
+
+    def downcase_email
+        self.email = email.downcase
+    end
 end
