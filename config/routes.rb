af3978628e1548efe79d0f2dc3aa6e8e4c324174
duongplayer99 <ho.khanh.duong99@gmail.com>
2024-01-25 18:58:21 +0900

    chuong 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/signup', to: 'users#new'
   resources :posts
   resources :users
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
