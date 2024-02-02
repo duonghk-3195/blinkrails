a63fcf427e601f6384ce82223da8e50d047a7ce3
duongplayer99 <ho.khanh.duong99@gmail.com>
2024-02-02 11:05:40 +0900

    add I18n

--- config/application.rb
+++ config/application.rb
@@ -4,16 +4,16 @@ require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
-config.i18n.available_locales = [:en, :vi, :ja]
-config.i18n.default_locale = :en
+
 Bundler.require(*Rails.groups)
 
 module Blink
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi, :ja]
+    config.i18n.default_locale = :en
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
