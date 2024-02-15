49957c5833f26562afcf1036275f95bcca2d9688
duongplayer99 <ho.khanh.duong99@gmail.com>
2024-02-15 16:34:12 +0700

    delegate done

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
     validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format"},
                       size: { less_than: 5.megabytes, message: "should be less than 5MB" }
 
+    delegate :name, to: :user, prefix: true
+
     def display_image
         image.variant(resize_to_limit: [300, 300]) # hien tai no khong resize duoc anh hien thi. se tim cach fix sau
     end
